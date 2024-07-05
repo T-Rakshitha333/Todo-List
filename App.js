069c25901a42b5b1cf50e4269990f8df6cc80191
T-Rakshitha333 <thatipamulavarshini11@gmail.com>
2024-07-05 09:22:26 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import TaskInput from './components/TaskInput';
+import TaskList from './components/TaskList';
+import './App.css'
+import { Container , Row, Col } from 'react-bootstrap';
+
+const App = () => {
+  return (
+   <div>
+    <Container>
+    <Row>
+      <Col lg={12} sm={12} md={12}>
+    <div className="App">
+      <h1 className='todos-heading'>To-Do List</h1>
+      <h1 class="create-task-heading">Create <span class="create-task-heading-subpart">Task</span></h1>
+      <TaskInput />
+      <TaskList />
+    </div>
+    </Col>
+    </Row>
+    </Container>
+    </div>
+  );
+};
+
+export default App;
